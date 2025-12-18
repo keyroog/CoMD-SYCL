7f17b1ed17ce125dda89f70de7ee7d0340297030
@s.sirica2000 <s.sirica2000@gmail.com>
2025-12-18 14:24:38 +0100

    Rimozione breakpoint

--- script-windows/run_comd_msys2.bat
+++ script-windows/run_comd_msys2.bat
@@ -1,16 +1,14 @@
 @echo off
-REM 1) oneAPI environment (x64)
+setlocal
+
+REM 1) oneAPI env
 call "C:\Program Files (x86)\Intel\oneAPI\setvars.bat" || exit /b 1
 
-REM 2) Prepend MSYS2 (UCRT64) paths
+REM 2) MSYS2 paths
 set "PATH=C:\msys64\ucrt64\bin;C:\msys64\usr\bin;%PATH%"
 
-REM 3) Build (bash login shell)
-C:\msys64\usr\bin\bash.exe -lc ^
-  "cd /c/Users/gamba/WSs/cWs/CoMD-SYCL/src-occl && \
-   uname -a && \
-   which make && which bash && \
-   which icpx || true && \
-   which mpicc || true && \
-   icpx --version && \
-   make -j"
+REM 3) repo path (modifica se serve)
+set "REPO=C:\Users\gamba\WSs\cWs\CoMD-SYCL"
+
+REM 4) run build in bash -lc (SINGLE LINE)
+C:\msys64\usr\bin\bash.exe -lc "set -e; cd '/c/Users/gamba/WSs/cWs/CoMD-SYCL/src-occl'; echo PWD=$(pwd); ls -la; which make; which icpx; icpx --version; make -j"
